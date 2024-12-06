e9de13cc89fa9ac84e8920c835b6a9146536d001
Monmon <syu24059@gmail.com>
2024-12-06 15:14:46 +0900

    Update details

---
+++ dex-backend/approve.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const { ethers } = require("ethers");
+
+// load key and Ganache URL from .env
+const privateKey = process.env.PRIVATE_KEY;
+const provider = new ethers.providers.JsonRpcProvider(process.env.GANACHE_URL);
+const wallet = new ethers.Wallet(privateKey, provider);
+
+// ABI and address of ERC20 tokens
+const tokenABI = [
+  "function mint(address to, uint256 amount) public",
+  "function approve(address spender, uint256 amount) public returns (bool)",
+];
+
+const TokenAAddress = "0xD66e16d5ee57FB55f36b5F4FeB6da1922AF6DDA7";
+const TokenBAddress = "0x590150D5BB3059E2f18Ec5CE136a839d97E9C1c5";
+const TokenCAddress = "0xA75199d79CD32dd8B2942F36C01dB7a8Bde2351d";
+
+async function main() {
+  // Get tokens instance
+  const TokenA = new ethers.Contract(TokenAAddress, tokenABI, wallet);
+  const TokenB = new ethers.Contract(TokenBAddress, tokenABI, wallet);
+  const TokenC = new ethers.Contract(TokenCAddress, tokenABI, wallet);
+
+  // Target Address and minting amount
+  const recipientAddress = "0x0162d27D2B11da01D59df10994D104e6B94FF5B0"; // User amount(ganache)
+  const amountToMint = ethers.utils.parseUnits("10000", 18); // mint 10000
+
+  // Approve the dex platform to user the tokens
+  const dexAddress = "0x14024406B95E8f821B1020Bb7fab45a18f863Edf"; // The address of DEX platform
+  const approveTxA = await TokenA.approve(dexAddress, amountToMint);
+  await approveTxA.wait();
+  const approveTxB = await TokenB.approve(dexAddress, amountToMint);
+  await approveTxB.wait();
+  const approveTxC = await TokenC.approve(dexAddress, amountToMint);
+  await approveTxC.wait();
+  console.log(`Approved ${amountToMint} Token to DEX at ${dexAddress}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
--- dex-backend/mintAndApprove.js
+++ dex-backend/mintAndApprove.js
@@ -12,36 +12,38 @@ const tokenABI = [
   "function approve(address spender, uint256 amount) public returns (bool)",
 ];
 
-const token0Address = "0xE9187dF444B561336c143B04E3a292502eCa2F33"; // token0 合约地址
-const token1Address = "0xf226aD181540a36408b110988dF3c68A94335AB3"; // token1 合约地址
+//const token0Address = "0xE9187dF444B561336c143B04E3a292502eCa2F33"; // token0 合约地址
+//const token1Address = "0xf226aD181540a36408b110988dF3c68A94335AB3"; // token1 合约地址
+
+const tTokenAddress = "0x87a0ee49199346B2fcdF54FbCc9C1150d95a22fF";
 
 async function main() {
   // 获取 Token0 和 Token1 的合约实例
-  const token0 = new ethers.Contract(token0Address, tokenABI, wallet);
-  const token1 = new ethers.Contract(token1Address, tokenABI, wallet);
+  const tToken = new ethers.Contract(tTokenAddress, tokenABI, wallet);
+  //const token1 = new ethers.Contract(token1Address, tokenABI, wallet);
 
   // 目标地址和数量
   const recipientAddress = "0x0162d27D2B11da01D59df10994D104e6B94FF5B0"; // 替换为目标地址
   const amountToMint = ethers.utils.parseUnits("1000", 18); // 铸造1000个代币
 
   // 铸造代币
-  const mintTx0 = await token0.mint(recipientAddress, amountToMint);
+  const mintTx0 = await tToken.mint(recipientAddress, amountToMint);
   await mintTx0.wait();
   console.log(`Minted ${amountToMint} Token0 to ${recipientAddress}`);
 
-  const mintTx1 = await token1.mint(recipientAddress, amountToMint);
+  /*const mintTx1 = await token1.mint(recipientAddress, amountToMint);
   await mintTx1.wait();
-  console.log(`Minted ${amountToMint} Token1 to ${recipientAddress}`);
+  console.log(`Minted ${amountToMint} Token1 to ${recipientAddress}`);*/
 
-  // 授权 DEX 平台合约使用代币
-  const dexAddress = "0x090Bc3ff8116D285ad616aF3A9dF066B64b3126F"; // DEX 平台合约地址
-  const approveTx0 = await token0.approve(dexAddress, amountToMint);
+  // Approve the contract to use the tokens
+  const dexAddress = "0x090Bc3ff8116D285ad616aF3A9dF066B64b3126F"; // DEX contract address
+  const approveTx0 = await tToken.approve(dexAddress, amountToMint);
   await approveTx0.wait();
   console.log(`Approved ${amountToMint} Token0 to DEX at ${dexAddress}`);
 
-  const approveTx1 = await token1.approve(dexAddress, amountToMint);
+  /*const approveTx1 = await token1.approve(dexAddress, amountToMint);
   await approveTx1.wait();
-  console.log(`Approved ${amountToMint} Token1 to DEX at ${dexAddress}`);
+  console.log(`Approved ${amountToMint} Token1 to DEX at ${dexAddress}`);*/
 }
 
 main().catch((error) => {
